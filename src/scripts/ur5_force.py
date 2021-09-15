b474fc303cb98bbb048d763069a1a034e06bae74
Lupasic <obulichev@yandex.ru>
2021-09-15 18:47:49 +0300

    Add first subprograms of force based control

--- src/scripts/ur5_force.py
+++ src/scripts/ur5_force.py
@@ -6,7 +6,7 @@ from time import perf_counter
 from numpy import array, frexp, sin, cos, pi, linspace, random, matrix
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-from multiprocessing import Process, Value
+from multiprocessing import Process, Value, Array
 
 # https://github.com/LukeSkypewalker/URX-jupiter-notebook/blob/master/URX_notebook.ipynb
 
@@ -28,6 +28,11 @@ class UR5e_force:
         # modification trajectory tune params
         self.__vir_stiff = 500
         self.__freq_mod_traj = 100
+
+        # shared memory
+        self.__Xd_shared = Array('d',[0,0,0])
+        self.__dXd_shared = Array('d',[0,0,0])
+        self.__force_from_sensor = Value('d',0.0)
 
         # vel control parameters
         self.__freq_log = 100
@@ -80,41 +85,75 @@ class UR5e_force:
         dX_d_mod = [dX_g[0], dX_g[1], dz_g + ddelta]
         return X_d_mod, dX_d_mod
 
-    def vel_control(self, X_d, dX_d):
-        X_d = array(X_d)
-        dX_d = array(dX_d)
-        z_act = []
-        z_des = []
-        time = []
+    def __modify_z_g(self, F_d, X_g):
+        return X_g[2] - F_d/self.__vir_stiff
+
+    def _force_planner(self, X_g, dX_g, F_cur, F_d):
+        X_g[2] = self.__modify_z_g(F_d, X_g)
         try: 
             t0 = perf_counter()
             t1 = 0
+            self.__delta_prev = 0
+            while True:
+                t = perf_counter() -t0 
+                if t - t1 >1/self.__freq_mod_traj:
+                    X_d_mod, dX_d_mod = self.__modify_trajectory_based_on_force(X_g, dX_g, F_cur)
+                    self.__Xd_shared.value = X_d_mod
+                    self.__dXd_shared.value = dX_d_mod
+                    t1 = t
+                    print(f'{X_d_mod} {dX_d_mod}', end = '\r', flush = True)
+        except KeyboardInterrupt:
+            pass
+
+    def vel_control(self, X_d, dX_d):
+        X_d = array(X_d)
+        dX_d = array(dX_d)
+        dX_cur = None
+        x_act = []
+        x_des = []
+        time = []
+        eps = 0.01
+        try: 
+            t0 = perf_counter()
+            t1 = 0
+            fl = 0
             while True: 
                 t = perf_counter() -t0
                 state = self.rob.getl()
-
                 X_cur = array(state[:3])
                 U = dX_d + self.__k*(X_d - X_cur)
-
-                if t - t1 >1/self.__freq_log:
-            
-                    z_act.append(X_cur[2])
-                    z_des.append(X_d[2])
-                    time.append(t)
-                    t1 = t
-                    print(f'{z_act[-1].round(3)} {z_des[-1].round(3)} {z_des[-1].round(3) - z_act[-1].round(3)}', end = '\r', flush = True)
-
                 self.rob.speedx("speedl",[U[0],U[1],U[2],0,0,0], self.__max_operational_acc, 5)
                 
+                if t - t1 >1/self.__freq_log:
+                    if fl:
+                        dX_cur = (X_cur - x_act[-1])/(t-t1)
+                    if fl == 0:
+                        fl = 1
+                    x_act.append(X_cur)
+                    x_des.append(X_d)
+                    time.append(t)
+                    t1 = t
+                    print(f'{x_act[-1][2].round(3)} {x_des[-1][2].round(3)} {x_des[-1][2].round(3) - x_act[-1][2].round(3)} {np.linalg.norm((X_d-X_cur))}', end = '\r', flush = True)
+
+                
+                
+                if dX_cur is not None:
+                    if np.linalg.norm((X_d-X_cur)) < eps and np.linalg.norm(dX_d - dX_cur) < eps:
+                        # print("Mech")
+                        pass
+                        # raise(KeyboardInterrupt)
+                        # self.rob.speedx("speedl",[0,0,0,0,0,0], self.__max_operational_acc, 1)
+                        # raise(KeyboardInterrupt)
+
 
 
         except KeyboardInterrupt:
             self.rob.speedx("speedl",[0,0,0,0,0,0], self.__max_operational_acc, 1)
             print('Robot is stopped')
 
-            plt.plot(time, array(z_act), label="actual")
-            plt.plot(time, array(z_des), label="needed")
-            plt.plot(time, array(z_act) - array(z_des), label="error")
+            plt.plot(time, array(x_act), label=["actual x","actual y","actual z"])
+            plt.plot(time, array(x_des), label=["needed x","needed y","needed z"])
+            # plt.plot(time, array(x_act) - array(x_des), label="error")
             plt.title("Manipulator control, k = " + str(self.__k))
             plt.xlabel("time, sec")
             plt.ylabel("position, m")
@@ -128,12 +167,26 @@ class UR5e_force:
         pass
     
 
+def read_data_from_force_sensor(futek, received_data):
+    while True:
+        rec_val =  futek.raw2F(futek.readData(write_to_file=1))
+        if rec_val > 1:
+            received_data.value = rec_val
+        else:
+            received_data.value = 0
+
+
 if __name__ == '__main__':
     ur_robot = UR5e_force(enable_force = 0)
     print(ur_robot)
 
     X_cur = ur_robot.rob.getl()[:3]
-    X_cur[2] = 0.3
+    X_cur[2] = 0.5
+    X_cur[1] = 0.2
     X_d = X_cur
 
-    ur_robot.vel_control(X_d, [0,0,0])
+    # ur_robot.vel_control(X_d, [0,0,0])
+    ur_robot._force_planner(X_d,[0,0,0],0,2)
+    print("done")
+
+
